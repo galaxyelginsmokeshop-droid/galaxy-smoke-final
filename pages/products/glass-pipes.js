b772fa07adc8a665533a69e76c557c1338d81d83
galaxyelginsmokeshop-droid <galaxyelginsmokeshop@gmail.com>
2025-08-26 13:12:02 -0500

    Final product image update

--- pages/products/glass-pipes.js
+++ pages/products/glass-pipes.js
@@ -1,13 +1,1 @@
-import Image from "next/image";
-
-export default function GlassPipes() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Glass Pipes</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <Image src="/images/products/glass-pipes/glass-pipe-1.jpg" width={300} height={300} alt="Glass Pipe 1" />
-        <Image src="/images/products/glass-pipes/glass-pipe-2.jpg" width={300} height={300} alt="Glass Pipe 2" />
-      </div>
-    </div>
-  );
-}
+// glass-pipes.js - sample content
--- pages/products/nic-vape-devices.js
+++ pages/products/nic-vape-devices.js
@@ -1,13 +1,1 @@
-import Image from "next/image";
-
-export default function NicVapeDevices() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Nicotine Vape Devices</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <Image src="/images/products/nic-vape-devices/nic-vape-device-1.jpg" width={300} height={300} alt="Nicotine Vape Device 1" />
-        <Image src="/images/products/nic-vape-devices/nic-vape-device-2.jpg" width={300} height={300} alt="Nicotine Vape Device 2" />
-      </div>
-    </div>
-  );
-}
+// nic-vape-devices.js - sample content
